d7c13a71956817013cda47c2b1ebe93c4a51615d
louk0198 <110351232+louk0198@users.noreply.github.com>
2022-08-16 15:11:53 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -16,19 +16,19 @@ global.APIKeys = {
 global.emoji01 = ['🐼'] // 👈 Enter your Emoji 
 global.emoji02 = ['🦋'] //👈 Enter your Emoji
 global.notice = ` *Hi 🥰 I m*
-*ＮＯＩＺ Ξ*
-*I am 16 years old developer...*
-*From Sri Lanka 🇱🇰*
+*ســفــيــان إيــتاشــي😻 Ξ*
+*عــمــࢪي 18 ســنــة ...*
+*From MOROCCO😻⃢🥵*
 *I am a Learning.*
 *Not more..*
 *Also I am Student*
-*Follow My Facebook - https://www.facebook.com/zenoixnoize*`  // 👈 Enter Your details and Social Media Links and Anything
-global.owner =  ['+94711502119'] //👈  Enter Your number
-global.premium =  ['+94711502119'] //👈  Enter Your number
-global.ownernomer = '+94711502119' //👈  Enter Your number
+*Follow My 𝐘𝐎𝐔𝐓𝐎𝐔𝐁 - https://youtu.be/kIwbcOHeAbs*`  // 👈 Enter Your details and Social Media Links and Anything
+global.owner =  ['+212776258137'] //👈  Enter Your number
+global.premium =  ['+212776258137'] //👈  Enter Your number
+global.ownernomer = '+212776258137' //👈  Enter Your number
 global.ownername = 'ZENOI' //👈 Enter Your name
-global.botname = 'ＧＡＲＦＩＥＬＤ ＢＯＴ' //👈 Enter Your Bot name
-global.footer = '© 𝖯𝗈𝗐𝖾𝗋𝖾𝖽 𝖡𝗒 𝖦𝖺𝗋𝖿𝗂𝖾𝗅𝖽 𝖡𝗈𝗍' //👈 Enter Your Name
+global.botname = '𝐒𝐎𝐔𝐅𝐈𝐀𝐍𝐄 ＢＯＴ' //👈 Enter Your Bot name
+global.footer = '© 𝖯𝗈𝗐𝖾𝗋𝖾𝖽 𝖡𝗒 𝐒𝐎𝐔𝐅𝐈𝐀𝐍𝐄 𝖡𝗈𝗍' //👈 Enter Your Name
 global.ig = 'https://github.com/Zenoixnoize/GARFIELD-WHATSAPP-BOT-v8' // 👈 You Can change this your choice
 global.region = 'India, South India, Kerala' // 👈 You Can change this your choice
 global.sc = 'https://github.com/Zenoixnoize/GARFIELD-WHATSAPP-BOT-v8'
